d03c34ecfe7d4c2fb5b94fde971eb4cd0234b89a
Marcos López <74747123+Marcos-Lopez-de-la-Fuente@users.noreply.github.com>
2022-11-03 20:07:31 +0100

    Pequeños avances

--- app/src/main/java/com/example/app2048/PruebasActivity.java
+++ app/src/main/java/com/example/app2048/PruebasActivity.java
@@ -2,9 +2,13 @@ package com.example.app2048;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Size;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
@@ -14,6 +18,8 @@ public class PruebasActivity extends AppCompatActivity {
 
     private Button forXFor;
     private GridLayout gridMain;
+    private Drawable items;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,7 @@ public class PruebasActivity extends AppCompatActivity {
 
         this.forXFor = findViewById(R.id.forXfor);
         this.gridMain = findViewById(R.id.gridMain);
+        this.items = getDrawable(R.drawable.item);
 
 
         // Si pulsan el botón
@@ -52,12 +59,34 @@ public class PruebasActivity extends AppCompatActivity {
     // Método para crear e introducir los TextView dentro del GridLayout
     public void createTextViewIntoGridLayout(int column, int row) {
         TextView textView = new TextView(this);
+
         textView.setText("1");
+        textView.setBackground(this.items);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(40);
 
-        this.gridMain.addView(textView, new GridLayout.LayoutParams(
-                GridLayout.spec(row, GridLayout.CENTER),
-                GridLayout.spec(column, GridLayout.CENTER)));
 
+        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams
+                (GridLayout.spec(row, 1f),
+                        GridLayout.spec(column, 1f)
+                );
+
+
+        int margins = 20;
+
+        layoutParams.setMargins(margins, margins, margins, margins);
+
+        textView.setLayoutParams(layoutParams);
+
+
+        this.gridMain.addView(textView);
+
+
+    }
+
+
+    public void createTextViewToGridLayout() {
+        
     }
 
 }
